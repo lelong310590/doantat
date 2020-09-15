d79a033ceaf13a03ea7d85723c878105b69b87d0
Train Heartnet <longlengoc90@gmail.com>
2020-09-15 14:01:59 +0700

    Withdrawal Module
    Add base ui in sidebar menu

---
+++ dashboard/alucms/theme/resources/views/layouts/withdrawal.blade.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * withdrawal.blade.php
+ * Created by: trainheartnet
+ * Created at: 9/15/20
+ * Contact me at: longlengoc90@gmail.com
+ */
+
---
+++ dashboard/alucms/theme/src/Http/Controllers/ThemeWithdrawalController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ThemeWithdrawalController.php
+ * Created by: trainheartnet
+ * Created at: 9/15/20
+ * Contact me at: longlengoc90@gmail.com
+ */
+
+
+namespace AluCMS\Theme\Http\Controllers;
+
+
+class ThemeWithdrawalController
+{
+
+}
